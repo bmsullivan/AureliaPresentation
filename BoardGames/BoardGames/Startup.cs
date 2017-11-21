ac71bb26151ad5f6e909fbdd34526759d89b8987
Brian Sullivan <bmsullivan@gmail.com>
2017-11-20 22:00:16 -0600

    Add DbContext and Get endpoint

--- BoardGames/BoardGames/Startup.cs
+++ BoardGames/BoardGames/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoardGames.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +26,9 @@ namespace BoardGames
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services
+                .AddDbContext<BoardGamesDbContext>(x => 
+                    x.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BoardGames;Trusted_Connection=True;MultipleActiveResultSets=true"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
